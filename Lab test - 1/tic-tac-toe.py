ab8a2089d7994dd38a0187935202d0ff2ce1463b
Praneeth <bpraneeth2000@gmail.com>
2020-11-10 15:27:46 +0530

    output

--- Lab test - 1/tic-tac-toe.py
+++ Lab test - 1/tic-tac-toe.py
@@ -50,9 +50,9 @@ def play_game():
 # Display the game board to the screen
 def display_board():
   print("\n")
-  print(board[0] + " | " + board[1] + " | " + board[2] + "     1 | 2 | 3")
-  print(board[3] + " | " + board[4] + " | " + board[5] + "     4 | 5 | 6")
-  print(board[6] + " | " + board[7] + " | " + board[8] + "     7 | 8 | 9")
+  print(board[0] + " | " + board[1] + " | " + board[2])
+  print(board[3] + " | " + board[4] + " | " + board[5])
+  print(board[6] + " | " + board[7] + " | " + board[8])
   print("\n")
 
 
